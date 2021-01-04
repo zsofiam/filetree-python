6dbf8aac43c31e7b8cc73af136b046e1c044d662
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-01-04 10:44:07 +0100

    filetree.py corrected

--- filetree.py
+++ filetree.py
@@ -12,7 +12,7 @@ def print_decorline(chars, length, keep_whole=True):
     print(decorline)
 
 
-def filetree(root, path="", exep=[".py"]):
+""" def filetree2(root, path="", exep=[".py", ".md"]):
 
     # Stop if this is not a folder:
     if not os.path.isdir(root):
@@ -33,7 +33,39 @@ def filetree(root, path="", exep=[".py"]):
             arrow = "`---- "
         print("{:s}{:s}{:s}".format(path, arrow, files_filtered[i]))
         # Recursive call to print the content of sub-folders:
-        filetree("{:s}/{:s}".format(root, files_filtered[i-1]), newpath, exep)
+        filetree2("{:s}/{:s}".format(root, files_filtered[i-1]), newpath, exep) """
+
+
+def filetree(path="", prefix_spaces=""):
+    dir_marker = "|-- "
+    file_marker = "`-- "
+
+    # Get items in the current dir
+    entries = os.listdir(path)
+    #remove unwanted entries '.git' and '*.py'
+    filter_list = ['.git', '.py', '.md', '.vscode']
+    # Filtering out every filter list item from the original entries list with this for loop
+    for elem in filter_list:
+        #Create a new entries list based on old one
+        entries = [ item for item in entries if elem not in item ]
+
+    # Stop:if the current dir does not contain newer entries
+    """ if len(entries) == 0:
+        return """
+    # Get files in current folder:
+    files = [ entry for entry in entries if os.path.isfile(os.path.join(path, entry)) ]
+    files = sorted(files)
+
+    for file in files:
+        print(f"{prefix_spaces}{file_marker}{file}")
+
+    # Get dirs in current folder:
+    dirs = [ entry for entry in entries if os.path.isdir(os.path.join(path, entry)) ]
+    dirs = sorted(dirs)
+    for dir in dirs:
+        print(f"{prefix_spaces}{dir_marker}{dir}")
+        new_path = os.path.join(path, dir)
+        filetree(path=new_path, prefix_spaces=prefix_spaces+4*' ')
 
 
 def main():
