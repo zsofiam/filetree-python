942070f9e3adf3356df30bfb9fdb600cc16ccab3
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-01-12 17:44:36 +0100

    finish debugging filetree

--- filetree.py
+++ filetree.py
@@ -20,10 +20,12 @@ def print_decorline(chars, length, keep_whole=True):
         return
 
     # Get files in current folder:
-    files = sorted(os.listdir(root))
-    files_filtered = [item for item in files if (not item.startswith(".") and
-                                                 not os.path.splitext(item)[1] in path)]
-    nfiles = len(files_filtered)
+    files_and_dirs = sorted(os.listdir(root))
+    files_and_dirs_filtered = [item for item in files_and_dirs if (not item.startswith(".") and
+                                                 not os.path.splitext(item)[1] in exep)]
+    nfiles = len(files_and_dirs_filtered)
+    files_filtered = [elem for elem in files_and_dirs_filtered if os.path.isfile(os.path.join(path, elem))]
+    dirs_filtered = [elem for elem in files_and_dirs_filtered if os.path.isdir(os.path.join(path, elem))]
 
     # Print the content of the current folder:
     newpath = path + "|   "
@@ -32,37 +34,32 @@ def print_decorline(chars, length, keep_whole=True):
         if i == nfiles - 1:
             newpath = path + "    "
             arrow = "`---- "
-        print("{:s}{:s}{:s}".format(path, arrow, files_filtered[i]))
+        print("{:s}{:s}{:s}".format(path, arrow, files_and_dirs_filtered[i]))
         # Recursive call to print the content of sub-folders:
-        filetree2("{:s}/{:s}".format(root, files_filtered[i-1]), newpath, exep) """
+        filetree2("{:s}/{:s}".format(root, files_and_dirs_filtered[i-1]), newpath, exep) """
 
 
-def filetree(path="", prefix_spaces=""):
-    dir_marker = "|-- "
-    file_marker = "`-- "
+def filetree(root, path="", exep=[".py", ".md"]):
 
-    # Get items in the current dir
-    entries = os.listdir(path)
-    #remove unwanted entries '.git' and '*.py'
-    filter_list = ['.git', '.py', '.md', '.vscode']
-    # Filtering out every filter list item from the original entries list with this for loop
-    for elem in filter_list:
-        #Create a new entries list based on old one
-        entries = [ item for item in entries if elem not in item ]
+    # Stop if this is not a folder:
+    if not os.path.isdir(root):
+        return
+
     # Get files in current folder:
-    files = [ entry for entry in entries if os.path.isfile(os.path.join(path, entry)) ]
-    files = sorted(files)
+    files_and_dirs = sorted(os.listdir(root))
+    files_and_dirs_filtered = [item for item in files_and_dirs if (not item.startswith(".") and
+                                                 not os.path.splitext(item)[1] in exep)]
+    length_of_files_and_dirs_filtered = len(files_and_dirs_filtered)
 
-    for file in files:
-        print(f"{prefix_spaces}{file_marker}{file}")
-
-    # Get dirs in current folder:
-    dirs = [ entry for entry in entries if os.path.isdir(os.path.join(path, entry)) ]
-    dirs = sorted(dirs)
-    for dir in dirs:
-        print(f"{prefix_spaces}{dir_marker}{dir}")
-        new_path = os.path.join(path, dir)
-        filetree(path=new_path, prefix_spaces=prefix_spaces + (4*' '))
+    # Print the content of the current folder:
+    newpath = path + "|   "
+    arrow = "|-- "
+    for i in range(length_of_files_and_dirs_filtered):
+        if i == length_of_files_and_dirs_filtered - 1:
+            newpath = path + "    "
+            arrow = "`-- "
+        print("{:s}{:s}{:s}".format(path, arrow, files_and_dirs_filtered[i]))
+        filetree("{:s}/{:s}".format(root, files_and_dirs_filtered[i]), newpath, exep)
 
 
 def main():
